cc7cd19c4fec8165d039f408ed79fe8040e9a604
Reeshma Hassen <r.hassen@ism-apac.com>
2018-10-07 20:04:47 +0530

    Configured payapal payment

--- src/app/components/grn-view/grn-view.component.ts
+++ src/app/components/grn-view/grn-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Grn } from '../../models/grn';
 import { GrnService } from '../../services/grn.service';
 
+declare let paypal: any; 
 @Component({
   selector: 'app-grn-view',
   templateUrl: './grn-view.component.html',
@@ -11,9 +12,58 @@ export class GrnViewComponent implements OnInit {
 
   goodsReceivedNotes: Grn[];
   selectedGrn: Grn;
+  model = new Grn(null,null,null,null,null,null);
+  modalOpened;
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
+  finalAmount: number = 1;
+  totalPrice;
+
 
   constructor(private grnService: GrnService) {
+  
+  }
 
+  
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AffkB-NAiPqW9vcjqkxp_jnOfoWHQRiSVYIJzyn88-WIOJvlghk5Ocg1YOv81HffiHFpeo9bymj1qyI_',
+      production: '<your-production-key here>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.selectedGrn.totalPrice, currency: 'USD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //this.selectedGrn.paymentStatus=true;
+      })
+    }
+  };
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
   }
 
   ngOnInit(): void {
@@ -27,8 +77,12 @@ export class GrnViewComponent implements OnInit {
       });
   }
 
-  makePayment() {
-    console.log('payment for ' + JSON.stringify(this.selectedGrn));
-  }
+  // makePayment() {
+  //   console.log('payment for ' + JSON.stringify(this.selectedGrn));
+  //   this.model = JSON.parse(JSON.stringify(this.selectedGrn)) as Grn
+  //   this.finalAmount = this.selectedGrn.totalPrice;
+  //   console.log(this.finalAmount);
+    
+  // }
 
 }
